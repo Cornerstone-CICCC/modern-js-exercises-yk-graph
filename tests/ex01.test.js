0cbd0a30c086321ee99104d8d8604b22742f4b20
yk-graph <yokokura.tatsuya@gmail.com>
2026-01-27 12:37:12 -0800

    refactor: Exercise01

--- tests/ex01.test.js
+++ tests/ex01.test.js
@@ -3,9 +3,11 @@ const { sumLargestNumbers, ErrorMessages } = require('../exercises/ex01');
 test("adds 2 largest numbers from array of 2 elements", () => {
   expect(sumLargestNumbers([1, 10])).toBe(11);
 });
+
 test("adds 2 largest numbers from array of 3 elements", () => {
   expect(sumLargestNumbers([1, 2, 3])).toBe(5);
 });
+
 test("adds 2 largest numbers from array of multiple elements", () => {
   expect(sumLargestNumbers([10, 4, 34, 6, 92, 2])).toBe(126);
 });
@@ -18,14 +20,26 @@ test("returns error message when input is missing", () => {
   expect(sumLargestNumbers()).toBe(ErrorMessages.NO_INPUT);
 });
 
-test("returns error message when input is not an array", () => {
+test("returns error message when input is empty string", () => {
   expect(sumLargestNumbers('')).toBe(ErrorMessages.NOT_ARRAY);
+});
+
+test("returns error message when input string", () => {
   expect(sumLargestNumbers('aaa')).toBe(ErrorMessages.NOT_ARRAY);
+});
+
+test("returns error message when input 0", () => {
   expect(sumLargestNumbers(0)).toBe(ErrorMessages.NOT_ARRAY);
+});
+
+test("returns error message when input number", () => {
   expect(sumLargestNumbers(5)).toBe(ErrorMessages.NOT_ARRAY);
 });
 
-test("returns error message when input array has less than two elements", () => {
+test("returns error message when input array has one element", () => {
   expect(sumLargestNumbers([5])).toBe(ErrorMessages.INSUFFICIENT_ELEMENTS);
+});
+
+test("returns error message when input array is empty", () => {
   expect(sumLargestNumbers([])).toBe(ErrorMessages.INSUFFICIENT_ELEMENTS);
-})
+});
